842a3821cf57998e65eb539d4bf26dce852288d1
Nenad Vukanovic <nesho97@gmail.com>
2017-11-11 09:49:02 +0100

    test

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+
+if(isset($_POST['username']))
+    echo "I received it";
+
+
+//var_dump($_POST);
+//var_dump($_SERVER['REQUEST_METHOD']);
+/*
+echo $_POST['username']." je ".$_POST['password'];*/
